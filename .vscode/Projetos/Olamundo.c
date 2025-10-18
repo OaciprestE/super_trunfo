2f3e6c0302db3b2d274c16e4e53556134489cf5a
Caio acipreste <caioacipreste@gmail.com>
2025-10-18 14:07:22 -0300

    adicionado densidade e pib percapta de ambas cartas

--- .vscode/Projetos/Olamundo.c
+++ .vscode/Projetos/Olamundo.c
@@ -8,6 +8,8 @@ int main(){
     float area1;
     float pib1;
     int pontos1;
+    float densidade1;
+    float percapta1;
     char estado2[30];
     char codigo2[20];
     char cidade2[30];
@@ -15,6 +17,8 @@ int main(){
     float area2;
     float pib2;
     int pontos2;
+    float densidade2;
+    float percapta2;
     
 
     printf("Aqui voce colocara informacoes sobre a carta numero 1. \n Qual o seu estado ?\n");
@@ -61,25 +65,37 @@ int main(){
     
     printf("quantos pontos turisticos tem o estado ?\n");
         scanf("%d", &pontos2);
-    
+
+        densidade1=populacao1/area1;
+        densidade2=populacao2/area2;
+        percapta1= pib1/populacao1;
+        percapta2= pib2/populacao2;
+
+
+
     printf("Voce completou as informacoes, segue abaixo as iformacoes das cartas\n");  
-    printf("estado da carta 1: %s\n", estado1);
-    printf("codigo da carta 1: %s\n", codigo1);
-    printf("cidade da carta 1: %s\n", cidade1);
-    printf("populacao da carta 1: %d\n", populacao1);
-    printf("tamanho da carta 1: %.2f\n", area1);
+    printf("Estado da carta 1: %s\n", estado1);
+    printf("Codigo da carta 1: %s\n", codigo1);
+    printf("Cidade da carta 1: %s\n", cidade1);
+    printf("Populacao da carta 1: %d\n", populacao1);
+    printf("Tamanho da carta 1: %.2f\n", area1);
     printf("Pib da carta 1: %.2f\n", pib1);
-    printf("pontos turisticos da carta 1: %d\n", pontos1);
-    printf("estado da carta 2: %s\n", estado2);
-    printf("estado da carta 2: %s\n", estado2);
-    printf("codigo da carta 2: %s\n", codigo2);
-    printf("cidade da carta 2: %s\n", cidade2);
-    printf("populacao da carta 2: %d\n", populacao2);
-    printf("tamanho da carta 2: %.2f\n", area2);
+    printf("Pontos turisticos da carta 1: %d\n", pontos1);
+    printf("Densidade populacional carta 1: %.2f\n", densidade1);
+    printf("Pib per capta carta 1: %.2f\n", percapta1);
+    printf("Estado da carta 2: %s\n", estado2);
+    printf("Estado da carta 2: %s\n", estado2);
+    printf("Codigo da carta 2: %s\n", codigo2);
+    printf("Cidade da carta 2: %s\n", cidade2);
+    printf("Populacao da carta 2: %d\n", populacao2);
+    printf("Tamanho da carta 2: %.2f\n", area2);
     printf("Pib da carta 2: %.2f\n", pib2);
     printf("pontos turisticos da carta 2: %d\n", pontos2);
-
-        if (pib1 > pib2) {
+    printf("Densidade populacional carta 1: %.2f\n", densidade2);
+    printf("Pib per capta carta 2: %.2f\n", percapta2);
+        
+    
+    if (pib1 > pib2) {
     printf("carta 1 venceu com o pib de %.2f\n", pib1);
   } else {
     printf("carta 2 venceu com o pib de %.2f\n", pib2);
